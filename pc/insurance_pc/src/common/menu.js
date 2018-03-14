b0b97bb781a52f819cf00beed2a180f30c9489f8
Mrpaker <spectator_w@126.com>
2018-03-14 16:35:32 +0800

    pc端项目初始化

---
+++ pc/insurance_pc/src/common/menu.js
@@ -0,0 +1,76 @@
+import { isUrl } from '../utils/utils';
+
+const menuData = [{
+  name: '车险客户',
+  icon: 'user',
+  path: 'dashboard',
+  children: [{
+    name: '分析页',
+    path: 'analysis',
+  }, {
+    name: '监控页',
+    path: 'monitor',
+  }, {
+    name: '工作台',
+    path: 'workplace',
+    // hideInMenu: true,
+  }],
+}, {
+  name: '业绩统计',
+  icon: 'area-chart',
+  path: 'clientList',
+  // children: [{
+  //   // name: '查询表格',
+  //   path: 'table-list',
+  // }, {
+  //   name: '标准列表',
+  //   path: 'basic-list',
+  // }],
+}, {
+  name: '渠道管理',
+  icon: 'fork',
+  path: 'profile',
+  children: [{
+    // name: '基础详情页',
+    path: 'basic',
+  }, {
+    name: '高级详情页',
+    path: 'advanced',
+    authority: 'admin',
+  }],
+},{
+  name: '账户',
+  icon: 'user',
+  path: 'user',
+  authority: 'guest',
+  children: [{
+    name: '登录',
+    path: 'login',
+  }, {
+    name: '注册',
+    path: 'register',
+  }, {
+    name: '注册结果',
+    path: 'register-result',
+  }],
+}];
+
+function formatter(data, parentPath = '/', parentAuthority) {
+  return data.map((item) => {
+    let { path } = item;
+    if (!isUrl(path)) {
+      path = parentPath + item.path;
+    }
+    const result = {
+      ...item,
+      path,
+      authority: item.authority || parentAuthority,
+    };
+    if (item.children) {
+      result.children = formatter(item.children, `${parentPath}${item.path}/`, item.authority);
+    }
+    return result;
+  });
+}
+
+export const getMenuData = () => formatter(menuData);
---
+++ pc/insurance_pc/src/theme.js
@@ -0,0 +1,11 @@
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+module.exports = {
+  // 'primary-color': '#10e99b',
+  'card-actions-background': '#f5f8fa',
+
+  // 'menu-dark-arrow-color': '#fff',
+  // 'card-actions-background': '#f5f8fa',
+  // 'menu-dark-submenu-bg': '#000c17',
+  // 'menu-dark-highlight-color': '#fff',
+  // 'menu-dark-item-selected-bg': 'primary-color',
+};
